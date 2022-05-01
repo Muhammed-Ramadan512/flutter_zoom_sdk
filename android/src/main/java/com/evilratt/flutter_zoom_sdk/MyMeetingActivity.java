44629e1cdcb92736aabdc3bb72b62a48ed9eb3f9
Moaz El-sawaf <moazelsawaf@hotmail.com>
2022-05-01 04:26:56 +0200

    Embeded Zoom Meeting UI into Another Activity

---
+++ android/src/main/java/com/evilratt/flutter_zoom_sdk/MyMeetingActivity.java
@@ -0,0 +1,13 @@
+package com.evilratt.flutter_zoom_sdk;
+
+import us.zoom.sdk.MeetingActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MyMeetingActivity extends MeetingActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
